9cc3306ec54df595e2724b37b5398b414141a5e2
ZerhackerJagi <52002508+ZerhackerJagi@users.noreply.github.com>
2019-06-20 12:01:58 +0200

    Update Hauptmenue.java

--- src/gui/Hauptmenue.java
+++ src/gui/Hauptmenue.java
@@ -5,6 +5,7 @@ public class Hauptmenue {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Welt!");
+		System.out.println("Du bist doof");
 	}
 
 }
